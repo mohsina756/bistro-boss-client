473a460fd8be353bed88a8ffcd2ce8a71b94a970
mati <matee803@gmail.com>
2023-11-29 18:36:15 +0600

    first commit

---
+++ src/pages/Home/Details/Details.jsx
@@ -0,0 +1,19 @@
+
+import service from '../../../assets/home/chef-service.jpg'
+
+const Details = () => {
+    return (
+        <div className="hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Bistro Boss</h1>
+      <p className="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, libero accusamus laborum deserunt ratione dolor officiis praesentium! Deserunt magni aperiam dolor eius dolore at, nihil iusto ducimus incidunt quibusdam nemo</p>
+      
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default Details;
---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,16 @@
+import Banner from "../Banner/Banner";
+import Category from "../Category/Category";
+import Details from "../Details/Details";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <Category></Category>
+            <Details></Details>
+        </div>
+    );
+};
+
+export default Home;
